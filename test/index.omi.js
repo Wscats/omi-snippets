1bb166df4f5729b309ee6d56d29459c849fe7fe1
wscats <2587575267@qq.com>
2009-12-25 19:42:09 +0800

    update

--- test/index.omi.js
+++ test/index.omi.js
@@ -3,13 +3,10 @@ import { WeElement, define, h } from "omi";
 // import axios from 'axios';
 
 const myEno = class extends WeElement {
-  static css() {
-    return `div{color:red}div p{font-size:14px}
+  static css = `div{color:red}div p{font-size:14px}
 `;
-  }
-
   render() {
-    return h("div", null, this.data.name, "12");
+    return h("div", null, this.data.name, "12345");
   }
 
   install() {
