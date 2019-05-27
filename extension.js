536f2f2286f662d2cd0f62da0f76259442fbb79f
wscats <2587575267@qq.com>
2019-05-27 22:04:27 +0800

    vsce package

--- extension.js
+++ extension.js
@@ -2,13 +2,17 @@ const vscode = require('vscode');
 const fs = require('fs');
 const path = require('path');
 const omil = require('omil');
+const prettier = require('prettier')
 
 const readFileContext = (path) => {
     return fs.readFileSync(path).toString();
 }
 
 const writeFileContext = (path, data) => {
-    return fs.writeFile(`${path}.jsx`, data, () => {
+    const code = prettier.format(data, {
+        parser: "babel",
+    });
+    fs.writeFile(`${path}.jsx`, code, () => {
         console.log('写入成功')
     });
 }
