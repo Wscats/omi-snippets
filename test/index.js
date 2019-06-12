c85b58a6112bb549fa66040cc7d8a0449cebbb89
wscats <2587575267@qq.com>
2019-06-12 11:25:11 +0800

    update snippets

--- test/index.js
+++ test/index.js
@@ -1,3 +1,43 @@
-module.exports = function(){
-    console.log(2)
-}
+import { WeElement, define, h } from "omi";
+
+// import axios from 'axios';
+// M
+const style = "a{color:red}";
+
+const myEno = class extends WeElement {
+  render() {
+    return h(
+      "div",
+      null,
+      this.data.name,
+      h(
+        "button",
+        {
+          onClick: this.testClick.bind(this)
+        },
+        "ok"
+      )
+    );
+  }
+
+  static css =
+    `
+    div{
+        color:red;
+        p{
+            font-size:14px;
+        }
+    }
+` + style;
+  install() {
+    this.data = {
+      name: "123"
+    };
+  }
+  testClick() {
+    this.data.name = 456;
+    this.update();
+  }
+};
+
+define("my-eno", myEno);
